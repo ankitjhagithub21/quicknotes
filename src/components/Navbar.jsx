c2626973debd60358618f7e4a7d0fc0d696b0453
ankitjhagithub21 <ajha45075@gmail.com>
2025-02-02 17:02:25 +0530

    fix overflow

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,7 +5,7 @@ import { CircleX, Github, House, Menu, Plus } from 'lucide-react'
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false)
   return (
-    <nav>
+    <nav className='relative'>
       <div className="container mx-auto flex items-center justify-between px-3 py-5">
         <h2 className='text-2xl font-semibold'>Quick<span className='text-green-500'>Notes</span> </h2>
         <ul className='md:flex hidden gap-4 items-center'>
@@ -21,7 +21,7 @@ const Navbar = () => {
           </a>
         </ul>
         <Menu className='md:hidden block' onClick={()=>setIsOpen(true)}/>
-        <div className={`absolute md:hidden block ${isOpen ? 'translate-x-0':'translate-x-full'}  transition duration-300 ease-in-out h-screen top-0 flex flex-col  p-5 gap-5 bg-black z-50  right-0`}>
+        <div className={`fixed md:hidden block ${isOpen ? 'translate-x-0':'translate-x-full'}  transition duration-300 ease-in-out h-screen top-0 flex flex-col  p-5 gap-5 bg-black z-50  right-0`}>
           <CircleX onClick={()=>setIsOpen(false)}/>
           <Link to={"/"} onClick={()=>setIsOpen(false)} className='bg-green-500 hover:bg-green-600 text-white flex items-center gap-1 rounded-lg px-3 text-sm py-1'>
             <House size={16} />
