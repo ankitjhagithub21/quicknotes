6f952d0fc813a56900b1aca878a4c066d4363081
ankitjhagithub21 <ajha45075@gmail.com>
2025-02-02 10:50:47 +0530

    added github icon

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,22 +1,25 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import { House, Plus } from 'lucide-react'
+import { Github, House, Plus } from 'lucide-react'
 
 const Navbar = () => {
   return (
     <nav>
-        <div className="container mx-auto flex items-center justify-between px-3 py-5">
-            <h2 className='text-2xl font-semibold'>Quick<span className='text-green-500'>Notes</span> </h2>
-          <ul className='flex gap-2'>
-         
-          <Link to={"/"} className='bg-green-500 hover:bg-green-600 text-white flex gap-1 rounded-lg px-4 py-2'>
-          <House/>
-          Home</Link>
-          <Link to={"/add"} className='bg-green-500 hover:bg-green-600 flex gap-1 text-white rounded-lg px-4 py-2'>
-          <Plus/>
-          Add Note</Link>
-          </ul>
-        </div>
+      <div className="container mx-auto flex items-center justify-between px-3 py-5">
+        <h2 className='text-2xl font-semibold'>Quick<span className='text-green-500'>Notes</span> </h2>
+        <ul className='flex gap-4 items-center'>
+
+          <Link to={"/"} className='bg-green-500 hover:bg-green-600 text-white flex items-center gap-1 rounded-lg px-3 text-sm py-1'>
+            <House size={16} />
+            Home</Link>
+          <Link to={"/add"} className='bg-green-500 hover:bg-green-600 flex gap-1 text-white items-center rounded-lg text-sm px-3 py-1'>
+            <Plus size={16}/>
+            Add Note</Link>
+          <a href="https://github.com/ankitjhagithub21/quicknotes" target='_blank'>
+            <Github />
+          </a>
+        </ul>
+      </div>
     </nav>
   )
 }
